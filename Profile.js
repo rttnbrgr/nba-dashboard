12d3d932bb4378da30cfbc49ee7345fdfcb75164
Nathan Rothenberger <nroth10@gmail.com>
2016-07-25 13:25:12 -0700

    Add Profile component

---
+++ Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Profile = React.createClass({
+	render: function() {
+		return (
+			<div className="navbar navbar-dark">
+				<button className="navbar-toggle">☰</button>
+			</div>
+		)
+	}
+})
+
+module.exports = Profile;
