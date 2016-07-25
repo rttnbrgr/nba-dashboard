39ec1f69faa7441691fb52aaa30474f71d8d651c
Nathan Rothenberger <nroth10@gmail.com>
2016-07-25 13:48:56 -0700

    Add sample data file

---
+++ data-nba.js
@@ -0,0 +1,102 @@
+
+
+damian	= [ 19.0, 20.7, 21.0, 25.1 ]
+steph 	= [ 22.9, 30.1, 23.8, 24.0 ]
+russell	= [ 23.2, 21.8, 28.1, 23.5 ]
+harden 	= [ 25.9, 25.4, 27.4, 29.0 ]
+
+19 - 31
+
+31
+27
+23	
+19
+
+/////////////////////////////////////////////
+
+var steph = [];
+steph = {
+	2016: {
+		pts: 30.1
+	}
+}
+
+steph
+30.1
+23.8
+24.0
+22.9
+
+"s12-13": {
+	points: 22.9,
+	rebounds: 4.0,
+	assists: 6.9
+},
+"s13-14": {
+	points: 24.0,
+	rebounds: 4.3,
+	assists: 8.5
+},
+"s14-15": {
+	points: 23.8,
+	rebounds: 4.3,
+	assists: 7.7
+},
+"s15-16": {
+	points: 30.1,
+	rebounds: 5.4,
+	assists: 6.7	
+},
+
+
+
+var russel = {};
+russell = {
+	
+	"s12-13": { 
+		points: 23.2,
+		rebounds: 5.2
+		assists: 7.4,
+	},
+	"s13-14": { 
+		points: 21.8,
+		rebounds: 5.7
+		assists: 6.9,
+	},
+	"s14-15": { 
+		points: 28.1,
+		rebounds: 7.3
+		assists: 8.6,
+	},
+	"s15-16": { 
+		points: 23.5,
+		rebounds: 7.8
+		assists: 10.4,
+	}
+}
+
+harden = {};
+harden = {
+
+	"s12-13": {
+		points: 25.9,
+		rebounds: 4.9,
+		assists: 5.8
+	}
+	"s13-14": {
+		points: 25.4,
+		rebounds: 4.7,
+		assists: 6.1
+	},
+	"s14-15": {
+		points: 27.4,
+		rebounds: 5.7,
+		assists: 7.0
+	},
+	"s15-16": {
+		points: 29.0,
+		rebounds: 6.1,
+		assists: 7.5
+	}
+
+}
